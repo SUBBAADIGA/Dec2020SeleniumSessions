ab78be72251b2c5c36a582f44ffdff1e445fd3bf
Subrahmanya Adiga <sadiga@Symphonysummit.com>
2021-03-13 16:11:25 +0530

    added from bash

--- src/main/java/Summit_test/addnew.java
+++ src/main/java/Summit_test/addnew.java
@@ -3,7 +3,7 @@ package Summit_test;
 public class addnew {
 
 	public static void main(String[] args) {
-		System.out.println("add to git");
+		System.out.println("add to git update from bash");
 	}
 
 }
